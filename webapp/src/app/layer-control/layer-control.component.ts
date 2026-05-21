6cc539b857871f19bebf83b04077064f956ebd1d
Raybird <a691228@gmail.com>
2026-05-21 10:56:04 +0800

    fix: replace isLayerActive subscription leak with async pipe

--- webapp/src/app/layer-control/layer-control.component.ts
+++ webapp/src/app/layer-control/layer-control.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.state';
 import * as MapActions from '../store/actions/map.actions';
 import * as MapSelectors from '../store/selectors/map.selectors';
-import { Subscription, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layer-control',
@@ -12,11 +11,9 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./layer-control.component.css'],
   standalone: false
 })
-export class LayerControlComponent implements OnInit, OnDestroy {
+export class LayerControlComponent implements OnInit {
   activeLayers$!: Observable<string[]>;
   isOpen = false;
-
-  private subscriptions: Subscription[] = [];
 
   availableLayers = [
     { id: 'terrain', name: '地形圖', description: '顯示海拔和地形資訊' },
@@ -33,21 +30,8 @@ export class LayerControlComponent implements OnInit, OnDestroy {
     this.activeLayers$ = this.store.select(MapSelectors.selectMapActiveLayers);
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
-  }
-
   toggleLayer(layerId: string): void {
     this.store.dispatch(MapActions.toggleMapLayer({ layerId }));
-  }
-
-  isLayerActive(layerId: string): boolean {
-    let isActive = false;
-    const sub = this.activeLayers$.subscribe(layers => {
-      isActive = layers.includes(layerId);
-    });
-    this.subscriptions.push(sub);
-    return isActive;
   }
 
   togglePanel(): void {
